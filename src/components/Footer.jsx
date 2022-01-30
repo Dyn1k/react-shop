15f2fe6bb395f781c88064ba3d0763bd9aaa120b
Daniil <kruglov1299@yandex.ru>
2022-01-30 21:56:07 +0300

    gh-pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 function Footer() {
-    return(
+    return (
         <footer className="page-footer blue lighten-1">
             <div className="footer-copyright">
                 <div className="container">
                     © {new Date().getFullYear()} Copyright Text
-                    <a className="grey-text text-lighten-4 right" href="#">Repo</a>
+                    <a className="grey-text text-lighten-4 right" href="https://github.com/Dyn1k/react-shop"
+                       target="_blank" rel="noreferrer nofollow noopener">Repo</a>
                 </div>
             </div>
         </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 function Header() {
-    return(
+    return (
         <nav className={'blue'}>
             <div className="nav-wrapper">
                 <a href="#" className="brand-logo">React Shop</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="#">Repo</a></li>
+                    <li><a href="https://github.com/Dyn1k/react-shop"
+                           target="_blank" rel="noreferrer nofollow noopener">Repo</a></li>
                 </ul>
             </div>
         </nav>
